fe05f406376f2c926c09a61a69d9d1c7c5de601c
CaptionLuffy <159707662+CaptionLuffy@users.noreply.github.com>
2024-06-19 07:40:30 +0530

    Update __main__.py

--- TelegramBot/__main__.py
+++ TelegramBot/__main__.py
@@ -2,5 +2,5 @@ from TelegramBot import bot
 from TelegramBot.logging import LOGGER
 
 LOGGER(__name__).info("client successfully initiated....")
-if __name__ == "__main__":
-    bot.run()
+#if __name__ == "__main__":
+bot.run()
